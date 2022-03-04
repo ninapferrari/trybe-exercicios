ef09e4b1e6d51e2e7b6ff9093b2195066d9e3ae3
Nina Ferrari <ninapferrari@gmail.com>
2022-03-04 15:13:04 -0600

    7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,11 +1,25 @@
-const testingScope = escopo => {
-    if (escopo === true) {
-        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-        console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
-    } else {
-        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-        console.log(`${elseScope} Não devo ser utilizada fora meu escopo (else)`);
-    }
-}
+// const testingScope = escopo => {
+//     if (escopo === true) {
+//         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//         console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
+//     } else {
+//         let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//         console.log(`${elseScope} Não devo ser utilizada fora meu escopo (else)`);
+//     }
+// }
 
-testingScope(true);
+// testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const arrayCres = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+
+    return oddsAndEvens;
+}
+const result = arrayCres();
+console.log(`Eis aqui ${result} o array em ordem crescente !`);
